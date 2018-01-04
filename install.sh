1c12bff6e2a90b499848b63beb48560e8c4ea111
張少懷 <chillman9527@gmail.com>
2018-01-04 22:26:55 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
+# install bash-it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
+
+# add vimrc and tmux config
+cp .vimrc .tmux.conf ~
+
+# edit the bash config depending on macos or ubuntu
+if [ -f ~/.bash_profile ]; then
+    cp macos.bash ~/.bash_it/custom
+    sed -i '' 's/bobby/powerline/g' ~/.bash_profile
+    . ~/.bash_profile
+elif [ -f ~/.bashrc ]; then
+    sed -i 's/bobby/zork/g' ~/.bashrc
+    . ~/.bashrc
+fi
---
+++ macos.bash
@@ -0,0 +1,6 @@
+# add path for network utils and metasploit framework
+export PATH=$PATH:/usr/local/sbin:/opt/metasploit-framework/bin
+
+# sets up proper alias commands when called
+alias umount='diskutil unmount'
+alias ptt='ssh bbsu@ptt.cc'
