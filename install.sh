b7d80dc366166e86f5bb1efaa2f9403ff96abb1d
chillman9527 <chillman9527@gmail.com>
2018-01-16 16:11:01 +0800

    update

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
-# install bash-it
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # add vimrc and tmux config
 cp .vimrc .tmux.conf ~
 
 # edit the bash config depending on macos or ubuntu
-if [ -f ~/.bash_profile ]; then
-    cp macos.bash ~/.bash_it/custom
-    sed -i '' 's/bobby/powerline/g' ~/.bash_profile
-    . ~/.bash_profile
-elif [ -f ~/.bashrc ]; then
-    sed -i 's/bobby/powerline/g' ~/.bashrc
-    . ~/.bashrc
+if [ "$(uname -s)" == "Darwin" ]; then
+    cp macos.zsh ~/.oh-my-zsh/custom/
+    sed -i '' 's/robbyrussell/ys/g' ~/.zshrc
+elif [ "$(uname -s)" == "Linux" ]; then
+    sed -i 's/robbyrussell/ys/g' ~/.zshrc
 fi
+source ~/.zshrc
